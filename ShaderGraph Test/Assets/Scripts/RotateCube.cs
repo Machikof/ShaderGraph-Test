6b40ce64c96e352b37e16af839fc8ff934e0561e
machiko_mk2 <machikou@icloud.com>
2020-07-05 00:28:49 +0900

    リムライティングシェーダーを制作

---
+++ ShaderGraph Test/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    private float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateSpeed = Random.Range(0.1f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(rotateSpeed, rotateSpeed * 2f, rotateSpeed * 1.5f);
+    }
+}
